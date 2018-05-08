7289d0c98f9422203d9932103b8a1c2016fea23d
GremoryD <dima.lamashevski@gmail.com>
2018-05-08 22:49:33 +0300

    Created Buy system

---
+++ ASP_MVC_Metanit_Lesson1/Models/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MVC_Metanit_Lesson1.Models
+{
+    public class BookContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
