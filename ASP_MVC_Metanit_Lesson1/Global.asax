b5f50ee1df360fa4c5ad186d4523917be4f75a4f
GremoryD <dima.lamashevski@gmail.com>
2018-05-08 21:31:56 +0300

    Добавьте файлы проекта.

---
+++ ASP_MVC_Metanit_Lesson1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_MVC_Metanit_Lesson1.MvcApplication" Language="C#" %>
---
+++ ASP_MVC_Metanit_Lesson1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_MVC_Metanit_Lesson1.Startup))]
+namespace ASP_MVC_Metanit_Lesson1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
